bd9f10f512911d3579ab74decfad3cd65be7ac1d
amellemgn <55215308+amellemgn@users.noreply.github.com>
2020-11-08 14:45:24 -0500

    Started Tile class file

--- PROTOTYPE/scripts/script.js
+++ PROTOTYPE/scripts/script.js
@@ -1,1 +1,16 @@
 // script.js will host js components, including slider code
+window.onload = function(){
+
+  // let newTile = new Tile(parameters go here);
+
+  document.querySelector("#sliderColor").addEventListener("input",outputColorUpdate);
+//this is from Sabine's files - trying to adapt to our proj
+  function outputAlphaUpdate() {
+  	document.querySelector('#colorOut').value = this.value;
+    for(let i =0; i<movingShapes.shapeList.length; i++){
+      movingShapes.shapeList[i].a = this.value/100;
+      movingShapes.shapeList[i].box.style.background = "rgba("+movingShapes.shapeList[i].r+","+movingShapes.shapeList[i].g+","+movingShapes.shapeList[i].b+","+movingShapes.shapeList[i].a+")";
+      movingShapes.shapeList[i].letterC.style.color = "rgba(255,255,255,"+movingShapes.shapeList[i].a+")";
+
+    }
+}
--- PROTOTYPE/scripts/tile.js
+++ PROTOTYPE/scripts/tile.js
@@ -1,1 +1,14 @@
-// tile.js will host the tile class object
+// tile.js will host the tile class
+
+function Tile(image, r, g, b, a){
+  this.image = image;
+  this.box.classList.add("box");
+
+  this.r =r;
+  this.g =g;
+  this.b=b;
+  this.a=a;
+
+  this.box.style.background = "rgba("+this.r+","+this.g+","+this.b+","+this.a+")";
+  document.getElementById("map").appendChild(this.box);
+}
