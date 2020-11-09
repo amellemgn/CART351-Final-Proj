eb6cabdad758bcf52e589f8e1efe6c95f25c1640
JulesGalbraith <43045124+JulesGalbraith@users.noreply.github.com>
2020-11-08 20:47:55 -0500

    Added library + extended class
    Added svg.min.js, an svg rendering librar y- got at least one hexagram object working (created in tile.js), but for some reason can't access its attributes in script.js ......... frustrated lol

--- PROTOTYPE/scripts/tile.js
+++ PROTOTYPE/scripts/tile.js
@@ -1,14 +1,28 @@
 // tile.js will host the tile class
 
-function Tile(image, r, g, b, a){
-  this.image = image;
-  this.box.classList.add("box");
+
+// function Tile(image, r, g, b, a){
+
+function Tile(r, g, b, a){
+  // this.image = image;
+  // this.box.classList.add("box");
 
   this.r =r;
   this.g =g;
   this.b=b;
   this.a=a;
 
-  this.box.style.background = "rgba("+this.r+","+this.g+","+this.b+","+this.a+")";
-  document.getElementById("map").appendChild(this.box);
+//using https://svgjs.com/docs/3.0/shape-elements/#svg-polygon
+
+  this.display = function(){
+    let draw = SVG().addTo('body').size(300, 300);
+
+    let hex = draw.polygon("49.43 41.28 24.97 54.85 0.5 41.28 0.5 14.14 24.97 0.57 49.43 14.14 49.43 41.28");
+    hex.fill("rgba("+this.r+","+this.g+","+this.b+","+this.a+")");
+
+    let map = $("#map").append(hex);
+  }
+
+  // this.box.style.background = "rgba("+this.r+","+this.g+","+this.b+","+this.a+")";
+  // document.getElementById("map").appendChild(this.box);
 }
