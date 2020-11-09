7030f681d667681006d0cf154ed8e2b3ac5b971e
amellemgn <55215308+amellemgn@users.noreply.github.com>
2020-11-09 13:10:31 -0500

    Added a text input bar

--- PROTOTYPE/index.php
+++ PROTOTYPE/index.php
@@ -25,23 +25,25 @@
 
   <div class="wrapper">
 <!-- everything in the slider 'column' to the left of screen is in sliderContainer -->
-    <div class="sliderContainer">
+    <div class="inputContainer">
       <h3> Change Hex Values: </h3>
       <div class = "sliderColor">
-      <label for="sliderColor"> Primary Color </label>
-     <input type="range" min="1" max="50" value="25" class="slider" id="Color">
-     <output for="slideColor" id="colorOut">25</output>
-     </div>
-     <div class = "sliderColor2">
-     <label for="sliderColor2"> Secondary Color </label>
-    <input type="range" min="1" max="50" value="25" class="slider" id="Color2">
-    <output for="slideColor2" id="colorOut2">25</output>
-    </div>
-    <div class = "sliderNoise">
-    <label for="sliderNoise"> Noise </label>
-   <input type="range" min="1" max="50" value="25" class="slider" id="Noise">
-   <output for="slideNoise" id="NoiseOut">25</output>
-   </div>
+        <label for="sliderColor"> Primary Color </label>
+        <input type="range" min="1" max="50" value="25" class="slider" id="Color">
+        <output for="slideColor" id="colorOut">25</output>
+      </div>
+      <div class = "sliderColor2">
+        <label for="sliderColor2"> Secondary Color </label>
+        <input type="range" min="1" max="50" value="25" class="slider" id="Color2">
+        <output for="slideColor2" id="colorOut2">25</output>
+      </div>
+      <div class = "sliderNoise">
+        <label for="sliderNoise"> Noise </label>
+        <input type="range" min="1" max="50" value="25" class="slider" id="Noise">
+        <output for="slideNoise" id="NoiseOut">25</output>
+      </div>
+      <p><label> Text input: </label><input type = "text" size="24" maxlength = "40"  name = "text_input" required></p>
+      <p><input type = "submit" name = "submit" value = "send" id =buttonS /></p>
    </div>
 <!-- the map and tiles are in a column in the center of screen called the mapContainer -->
     <div class = "mapContainer" id = "mapContainer">
