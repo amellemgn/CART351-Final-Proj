b2e206a003fa39a7ad6c60a72dc1431a6a2ffbff
amellemgn <55215308+amellemgn@users.noreply.github.com>
2020-11-09 15:11:43 -0500

    New hexagon example from Sabine without Honeycomb.

---
+++ HONEYCOMB/hexObj.js
@@ -0,0 +1,46 @@
+//hexObj takes three parameters. calculating each point's x, y coordinates using coordinates.
+// finding center of circles, calculating each x, y point
+
+function HexObj(centerX,centerY, lineLength){
+
+this.positions = [];
+this.centerX = centerX;
+this.centerY = centerY;
+this.lineLength = lineLength;
+
+const NUM_SIDES = 6;
+//make an array of positions
+//calculating six positions - dynamically.
+for(let i=0; i<NUM_SIDES; i++)
+{
+  //returns hex values in an array. 
+  this.positions.push(new PosVector(
+    flat_hex_cornerX(this.centerX, this.lineLength, i),
+    flat_hex_cornerY(this.centerY, this.lineLength, i)
+                              ));
+}
+
+}//end class
+
+/** HELPER **/
+  // to calculate an xpos
+  //helper functions - calculte x and y at a given angle. each point is 60 degrees apart.
+    function flat_hex_cornerX(cx, size, i){
+        let angle_deg = 60 * i;
+        // convert to radians
+        let angle_rad = Math.PI / 180 * angle_deg;
+        return (cx + size * Math.cos(angle_rad))
+
+      }
+  // to calculate a ypos
+      function flat_hex_cornerY(cy, size, i){
+        let angle_deg = 60 * i;
+        // convert to radians
+        let angle_rad = Math.PI / 180 * angle_deg;
+        return (cy + size * Math.sin(angle_rad))
+
+      }
+function PosVector(x,y){
+    this.x = x;
+    this.y = y;
+  }
---
+++ HONEYCOMB/myTestRev.js
@@ -0,0 +1,34 @@
+//https://svgjs.com/docs/3.0/getting-started/
+SVG.on(document, 'DOMContentLoaded', function() {
+  //use the SVG() function to create an SVG document and add it to the html page:
+const draw = SVG().addTo('#container').size('100%', '100%');
+// an SVG symbol can be reused
+
+//HexObj takes three parameters - center X, center Y, line length
+//instances of hexObj class - calculating position
+let hexobjA = new HexObj(100,100,50);
+let hexobjB = new HexObj(50,50,50);
+
+// representative of the svg itself.
+// showing seperation between having an array of points and then drawing them.
+const hexSymbol = draw.symbol()
+  // map the corners' positions to a string and create a polygon
+  //polyon function requires a string.
+  // .map basically takes the array of x, y positions and converts it into a string of values.
+  // basically converting for the svg format.
+  .polygon(hexobjA.positions.map(({ x, y }) => `${x},${y}`))
+   .fill('#ff0000')
+  .stroke({ width: 1, color: '#999' })
+    draw.use(hexSymbol);
+    //draw.use(hexSymbol).translate(50, 10);
+
+
+    const hexSymbolTwo = draw.symbol()
+      // map the corners' positions to a string and create a polygon
+      .polygon(hexobjB.positions.map(({ x, y }) => `${x},${y}`))
+       .fill('#000000')
+      .stroke({ width: 1, color: '#999' })
+     draw.use(hexSymbolTwo);
+        //draw.use(hexSymbol).translate(50, 10);
+
+});
