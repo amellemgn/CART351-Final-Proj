7c68c75678bfb979a278dac00b34febd07a3eada
JulesGalbraith <43045124+JulesGalbraith@users.noreply.github.com>
2020-11-10 01:03:54 -0500

    :)))))))
    1. Tile object that Sabine made largely unchanged
    2. Added CSS file
    3. Added Slider UIs
    4. Hooked 3/6 sliders to values that alter hex appearance and position
    5. Some other stuff probably

--- HONEYCOMB/hexObj.js
+++ HONEYCOMB/hexObj.js
@@ -1,19 +1,19 @@
 //hexObj takes three parameters. calculating each point's x, y coordinates using coordinates.
 // finding center of circles, calculating each x, y point
 
-function HexObj(centerX,centerY, lineLength){
+function HexObj(centerX,centerY){
 
 this.positions = [];
 this.centerX = centerX;
 this.centerY = centerY;
-this.lineLength = lineLength;
+this.lineLength = 50;
 
 const NUM_SIDES = 6;
 //make an array of positions
 //calculating six positions - dynamically.
 for(let i=0; i<NUM_SIDES; i++)
 {
-  //returns hex values in an array. 
+  //returns hex values in an array.
   this.positions.push(new PosVector(
     flat_hex_cornerX(this.centerX, this.lineLength, i),
     flat_hex_cornerY(this.centerY, this.lineLength, i)
--- HONEYCOMB/myTestRev.js
+++ HONEYCOMB/myTestRev.js
@@ -6,29 +6,75 @@ const draw = SVG().addTo('#container').size('100%', '100%');
 
 //HexObj takes three parameters - center X, center Y, line length
 //instances of hexObj class - calculating position
-let hexobjA = new HexObj(100,100,50);
-let hexobjB = new HexObj(50,50,50);
+let hexobjA = new HexObj(125,93);
+let hexobjB = new HexObj(50,50);
 
-// representative of the svg itself.
-// showing seperation between having an array of points and then drawing them.
-const hexSymbol = draw.symbol();
-  // map the corners' positions to a string and create a polygon
-  //polyon function requires a string.
-  // .map basically takes the array of x, y positions and converts it into a string of values.
-  // basically converting for the svg format.
-  .polygon(hexobjA.positions.map(({ x, y }) => `${x},${y}`))
-   .fill('#ff0000')
-  .stroke({ width: 1, color: '#999' })
-    draw.use(hexSymbol);
-    //draw.use(hexSymbol).translate(50, 10);
+let hexObjs = [hexobjA, hexobjB];
 
-
-    const hexSymbolTwo = draw.symbol()
-      // map the corners' positions to a string and create a polygon
-      .polygon(hexobjB.positions.map(({ x, y }) => `${x},${y}`))
-       .fill('#000000')
-      .stroke({ width: 1, color: '#999' })
-     draw.use(hexSymbolTwo);
+// // representative of the svg itself.
+// // showing seperation between having an array of points and then drawing them.
+//   const hexSymbol = draw.symbol()
+//   // map the corners' positions to a string and create a polygon
+//   //polyon function requires a string.
+//   // .map basically takes the array of x, y positions and converts it into a string of values.
+//   // basically converting for the svg format.
+//   .polygon(hexobjA.positions.map(({ x, y }) => `${x},${y}`))
+//    .fill('#ff0000')
+//   .stroke({ width: 1, color: '#999' })
+//     draw.use(hexSymbol);
+//     //draw.use(hexSymbol).translate(50, 10);
+//
+//
+//     const hexSymbolTwo = draw.symbol()
+//       // map the corners' positions to a string and create a polygon
+//       .polygon(hexobjB.positions.map(({ x, y }) => `${x},${y}`))
+//        .fill('#000000')
+//       .stroke({ width: 1, color: '#999' })
+//      draw.use(hexSymbolTwo);
         //draw.use(hexSymbol).translate(50, 10);
+    let huevalue;
+    let tileX;
+    let tileY;
+    // $('#hue').change(getHueValue);
+    $('#button').on('click',newOnClick);
+
+    let count = 0;
+    function newOnClick(){
+      console.log("hi");
+      //creating new hex object on button press - storing in empty variable to
+      //stash in an array
+      count ++;
+
+      console.log(count);
+
+      huevalue = document.getElementById("hue1").value;
+      console.log(huevalue);
+
+    let  xRange = document.getElementById("xRange").value;
+    let  yRange = document.getElementById("yRange").value;
+
+    let docWidth = document.getElementById('container').clientWidth;
+    let docHeight = document.getElementById('container').clientHeight;
+
+    tileX = xRange*docWidth;
+    tileY = xRange*docHeight;
+
+      console.log(tileX,tileY);
+      //we can make this more complex eventually
+      let newHex = new HexObj(tileX,tileY);
+      hexObjs.push(newHex);
+      console.log(newHex.lineLength);
+
+
+      let newHexSymbol = draw.symbol()
+      .polygon(newHex.positions.map(({ x, y }) => `${x},${y}`))
+       .fill('hsl('+huevalue+',100%,70%)')
+      .stroke({ width: 1, color: '#999' })
+     draw.use(newHexSymbol);
+   }
+//
+// function getHueValue(e){
+//   value = document.getElementById("hue1").value;
+// }
 
 });
